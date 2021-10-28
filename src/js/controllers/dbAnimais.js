24c1fe50b1714edc4bad592e12d601bf8c89d824
ThiagooCardoso <taugusto964@gmail.com>
2021-10-28 13:54:50 -0300

    input dieta

--- src/js/controllers/dbAnimais.js
+++ src/js/controllers/dbAnimais.js
@@ -14,7 +14,7 @@
  function Insert() { 
      //cria tabela
      dbObj.transaction(function (tx) {    
-         tx.executeSql('CREATE TABLE IF NOT EXISTS Cadastro_Animal (id integer primary key asc, Idade integer, Peso integer, Raça string not null, Sexo string, Data string)',
+         tx.executeSql('CREATE TABLE IF NOT EXISTS Cadastro_Animal (id integer primary key asc, Idade integer, Peso integer, Raça string not null, Sexo string,Dieta string, Data string)',
          [],
          function() {console.log("Tabela criada com sucesso!");},
          function(){alert("tabela não criada!")} 
@@ -26,10 +26,11 @@
      var peso = document.getElementById("weight").value;    
      var raça = document.getElementById("breed").value;  
      var sexo = document.getElementById("Sexo").value; 
+     var dieta = document.getElementById("Diet").value;
      var data = document.getElementById("DataEntrada").value
      dbObj.transaction(function (tx) {    
-         tx.executeSql('INSERT INTO Cadastro_Animal(Idade, Peso, Raça, Sexo, Data) values(?,?,?,?,?);', 
-         [idade,peso,raça,sexo,data],
+         tx.executeSql('INSERT INTO Cadastro_Animal(Idade, Peso, Raça, Sexo, Data) values(?,?,?,?,?,?);', 
+         [idade,peso,raça,sexo,dieta,data],
          null,
          null);
   
