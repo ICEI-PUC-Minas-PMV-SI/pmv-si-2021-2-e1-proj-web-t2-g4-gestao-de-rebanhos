a5de26824d34432b15c21903a99bd2eca46340c5
limamariana <62914189+limamariana@users.noreply.github.com>
2021-10-31 17:16:43 -0300

    Add files via upload
    Html, css e Js das páginas de baixa e cadastro de insumos

---
+++ src/views/insumos/baixa-insumos/baixa-insumos.js
@@ -0,0 +1,20 @@
+
+        function save(){
+            var spinner = document.querySelector("#spinner");
+            var table = document.querySelector("#table-response");
+            var total = document.querySelector("#total");
+            spinner.style.display = 'block';
+            $( "#btn-save" ).prop( "disabled", true );
+            $("#btn-save").text("Salvando");
+            setTimeout(function(){  setDisplay() }, 500);
+        }
+        function setDisplay(){
+            var spinner = document.querySelector("#spinner");
+            var table = document.querySelector("#table-response");
+            spinner.style.display = 'none';
+            table.style.display = 'block';
+            $("#total").text("3");
+            $("#btn-save").text("Salvar");
+            $( "#btn-save" ).prop( "disabled", false );
+
+        }
