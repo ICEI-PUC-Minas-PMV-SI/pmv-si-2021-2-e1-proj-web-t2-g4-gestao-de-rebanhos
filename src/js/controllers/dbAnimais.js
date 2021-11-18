70f6dda7c47fa6f7881c35f551372823bf553ee3
ThiagooCardoso <taugusto964@gmail.com>
2021-11-18 11:16:39 -0300

    inserido dietas no select

--- src/js/controllers/dbAnimais.js
+++ src/js/controllers/dbAnimais.js
@@ -1,24 +1,17 @@
      //Criar ou abrir database
-     var Database_Name = 'AnimaisDatabase';    
-     var Version = 1.0;    
-     var Text_Description = 'Database dos Animais';    
-     var Database_Size = 2 * 1024 * 1024;    
-     var dbObj = openDatabase(Database_Name, Version, Text_Description, Database_Size, OnSuccessCreate());  
+     import { db } from '../system_utilities/db.js';
  
-   
-            
- function OnSuccessCreate() {    
-     console.log('Database Created Sucessfully');    
- }  
  
    //cria tabela
-   dbObj.transaction(function (tx) {    
+   function criaTabelaAnimais(){
+   db.transaction(function (tx) {    
     tx.executeSql('CREATE TABLE IF NOT EXISTS Cadastro_Animal (id integer primary key asc, Idade integer, Peso integer, Raça string not null, Sexo string,Dieta string, Data string)',
     [],
     function() {console.log("Tabela criada com sucesso!");},
     function(){alert("tabela não criada!")} 
     )
 }); 
+   } 
  function Insert() { 
      
      //inserir dados na tabela
@@ -28,7 +21,7 @@
      var sexo = document.getElementById("Sexo").value; 
      var dieta = document.getElementById("Diet").value;
      var data = document.getElementById("DataEntrada").value
-     dbObj.transaction(function (tx) {    
+     db.transaction(function (tx) {    
          tx.executeSql('INSERT INTO Cadastro_Animal(Idade, Peso, Raça, Sexo,Dieta, Data) values(?,?,?,?,?,?);', 
          [idade,peso,raça,sexo,dieta,data],
          function(){console.log("Valores inseridos na tabela!!")},
---
+++ src/js/controllers/dietaCtrl.js
@@ -0,0 +1,100 @@
+import { db } from '../system_utilities/db.js';
+
+window.addEventListener('load', redy);
+
+function criarTabelaDietaseInsumos() {
+    var query = "CREATE TABLE IF NOT EXISTS dietas ( id TEXT,nome TEXT)";
+    var queryInsumos = "CREATE TABLE IF NOT EXISTS dietaInsumos (id INTEGER PRIMARY KEY, idInsumo INTEGER, idDieta TEXT, nomeInsumo TEXT, qtdInsumos REAL, duracao INTEGER)"
+    db.transaction(function(tx) {
+        tx.executeSql(query);
+        tx.executeSql(queryInsumos);
+    });
+}
+
+
+function generateUUID() { // Public Domain/MIT
+    var d = new Date().getTime(); //Timestamp
+    var d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now() * 1000)) || 0; //Time in microseconds since page-load or 0 if unsupported
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16; //random number between 0 and 16
+        if (d > 0) { //Use timestamp until depleted
+            r = (d + r) % 16 | 0;
+            d = Math.floor(d / 16);
+        } else { //Use microseconds since page-load if supported
+            r = (d2 + r) % 16 | 0;
+            d2 = Math.floor(d2 / 16);
+        }
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+}
+
+function save() {
+    //gera um id unico
+    var id = generateUUID();
+    var nome = document.getElementById('nomedieta').value;
+    var quantidade = document.getElementById('Quantidadeinsumos').value;
+
+    db.transaction(function(tx) {
+
+        tx.executeSql('INSERT INTO dietas (id, nome) VALUES (?, ?)', [id, nome],
+            //callback sucesso
+            function() {
+                for (var i = 1; i <= quantidade; i++) {
+                    var nomeInsumo = document.getElementById('supplyname' + i).value;
+                    var qtdInsumos = document.getElementById('quantity' + i).value;
+                    var duracao = document.getElementById('duration' + i).value;
+                    tx.executeSql('INSERT INTO dietaInsumos (idDieta, nomeInsumo, qtdInsumos, duracao) VALUES (?,?,?,?)', [id, nomeInsumo, qtdInsumos, duracao],
+                        //callback sucesso
+                        function() {
+
+                            swal.fire({
+                                icon: "success",
+                                title: "Dieta cadastrada com sucesso!",
+                            });
+
+                        }
+                    );
+                }
+            }
+        );
+
+
+    });
+}
+
+//selecionar das dietas
+function getDietas(callback){
+    db.transaction(function(tx){
+        tx.executeSql('SELECT id,nome, FROM dietas ORDER BY nome',[],
+        function(tx,resultado){
+            callback(resultado);
+        },
+        function(erro){
+            console.log(erro);
+        })
+    });
+}
+
+
+function redy() {
+    if (document.getElementById('btn-save')) {
+        document.getElementById('btn-save').addEventListener('click', save);
+    }
+    //if (document.getElementById('btn-search')) document.getElementById('btn-search').addEventListener('click', search);
+    criarTabelaDietaseInsumos();
+    console.log("Chamou controller");
+
+    getDietas(function(resultado){
+        console.log("Chamou getdietas");
+        $(resultado.rows).each(function(index,dados){
+            //console.log(resultado.rows);
+           // console.log(dados.nome);
+            let option = document.createElement('option');
+            option.value = dados.id;
+            option.innerHTML = dados.nome;
+    
+            $('#listaDietas').append(option); //adicionar objeto ao select
+        });
+    });
+
+}
