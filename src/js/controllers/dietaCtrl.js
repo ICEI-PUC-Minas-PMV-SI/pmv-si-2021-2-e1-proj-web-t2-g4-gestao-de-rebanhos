f87bc9bdcc0d871e2f2ca069a83375d5387ad15a
ThiagooCardoso <taugusto964@gmail.com>
2021-11-21 13:43:53 -0300

    validação

--- src/js/controllers/dietaCtrl.js
+++ src/js/controllers/dietaCtrl.js
@@ -2,11 +2,10 @@ import { db } from '../system_utilities/db.js';
 
 window.addEventListener('load', redy);
 
-window.addEventListener('load', redy);
 
 function criarTabelaDietaseInsumos() {
-    var query = "CREATE TABLE IF NOT EXISTS dietas ( id TEXT,nome TEXT)";
-    var queryInsumos = "CREATE TABLE IF NOT EXISTS dietaInsumos (id INTEGER PRIMARY KEY, idInsumo INTEGER, idDieta TEXT, nomeInsumo TEXT, qtdInsumos REAL, duracao INTEGER)"
+    var query = "CREATE TABLE IF NOT EXISTS dietas ( id varchar,nome varchar)";
+    var queryInsumos = "CREATE TABLE IF NOT EXISTS dietaInsumos (id INTEGER PRIMARY KEY, idDieta varchar , nomeInsumo varchar, qtdInsumos real, duracao integer)"
     db.transaction(function(tx) {
         tx.executeSql(query);
         tx.executeSql(queryInsumos);
@@ -32,37 +31,88 @@ function generateUUID() { // Public Domain/MIT
 
 function save() {
     //gera um id unico
+    console.log("chamou save");
     var id = generateUUID();
     var nome = document.getElementById('nomedieta').value;
     var quantidade = document.getElementById('Quantidadeinsumos').value;
     var dados = [id,nome];
+   // var ID = document.getElementById('ID').value;
 
+    var validacao = true;
+    var msgHtml = '';
+    var todospreenchidos= false;
+
+    if (nome.length <= 0) {
+        validacao = false;
+        msgHtml += '<p> - O campo <b>nome</b> é obrigatório.</p>';
+    }
+    if (quantidade.length <= 0) {
+        validacao = false;
+        msgHtml += '<p> - O campo <b>quantidade</b> é obrigatório.</p>';
+    }
+    
+
+    
+    if(validacao == true){
     db.transaction(function(tx) {
 
         tx.executeSql('INSERT INTO dietas (id, nome) VALUES (?, ?)', dados,
             //callback sucesso
             function() {
-                for (var i = 1; i <= quantidade; i++) {
+                console.log("entrou na função");
+                for (var i = 0; i < quantidade; i++) {
                     var nomeInsumo = document.getElementById('supplyname' + i).value;
                     var qtdInsumos = document.getElementById('quantity' + i).value;
                     var duracao = document.getElementById('duration' + i).value;
-                    tx.executeSql('INSERT INTO dietaInsumos (idDieta, nomeInsumo, qtdInsumos, duracao) VALUES (?,?,?,?)', [id, nomeInsumo, qtdInsumos, duracao],
+                    dados = [id,nomeInsumo,qtdInsumos,duracao];
+
+                    if (nomeInsumo.length <= 0) {
+                        validacao = false;
+                        msgHtml += '<p> - O campo <b>nome Insumo</b> é obrigatório.</p>';
+                    }
+                    if (qtdInsumos.length <= 0) {
+                        validacao = false;
+                        msgHtml += '<p> - O campo <b>quantidade</b> é obrigatório.</p>';
+                    }
+                    if (duracao.length <= 0) {
+                        validacao = false;
+                        msgHtml += '<p> - O campo <b>duração</b> é obrigatório.</p>';
+                    }
+                    if (validacao == true){
+                    tx.executeSql('INSERT INTO dietaInsumos (idDieta, nomeInsumo, qtdInsumos, duracao) VALUES (?,?,?,?)',dados,
                         //callback sucesso
-                        function() {
-
+                        function(tx,msg) {
+                            //modal para informar o usuario
                             swal.fire({
-                                icon: "success",
+                                 icon: "success",
                                 title: "Dieta cadastrada com sucesso!",
-                            });
-
-                        }
+                             });
+                        },
+                        function(tx,erro){console.log("valores não inseridos");console.log(erro);}
                     );
+                    }
+                    else {
+                        swal.fire({
+                            icon: "error",
+                            title: "Preencha os campos corretamente.",
+                            html: msgHtml
+                        });
+                    }
+                    
                 }
             }
         );
 
 
     });
+}
+else {
+    swal.fire({
+        icon: "error",
+        title: "Preencha os campos corretamente.",
+        html: msgHtml
+    });
+}
 }
 
 //selecionar das dietas
